2690fa00b7c681d6bf70b5e896ab6238253e7f0e
Zyklop <fzj890909@gmail.com>
2017-04-04 23:34:08 +0200

    add classes of forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import Email, Length, Regexp, Required, EqualTo
 from ..models import User
+from flask_login import current_user
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[Required(), Email(), Length(1, 64)])
@@ -28,3 +29,35 @@ class RegisterForm(FlaskForm):
     def validate_user_name(self, field):
         if User.query.filter_by(user_name=field.data).first():
             raise ValidationError('Username already in used.')
+
+class PasswordChangeForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[Required()])
+    new_password = PasswordField('New Password', validators=[Required(), EqualTo('new_password2', message='Password must be matched.'),
+                                Regexp('^(?!^[0-9a-z]+$)(?!^[0-9A-Z]+$)(?!^[a-zA-Z]+$)[a-zA-Z0-9]{6,20}$', 0, 
+                                'Password must have a combination from letters and numbers and the langth must be 6 to 20.')]) 
+    new_password2 = PasswordField('*Reenter your password', validators=[Required()])
+    submit = SubmitField('Confirm')
+    
+    def validate_old_password(self, field):
+        if not current_user.verify_password(field.data):
+            raise ValidationError('Invalid Password')
+            
+class EmailChangeForm(FlaskForm):
+    email = StringField('New Email', validators=[Reauired(), Email(), Length(1, 64)])
+    submit = SubmitField('Confirm')
+    
+    def validate_email(self, field):
+        if User.query.filter_by(Email=field.data).first():
+            raise ValidationError('Email already in used.')
+            
+class PasswordResetEmailForm(FlaskForm):
+    email = StringField('Your Login Email', validator=[Required(), Email(), Length(1, 64)])
+    submit = SubmitField('Send Token')
+    
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('New Password', validators=[Required(), EqualTo('password2', message='Password must be matched.'),
+                                Regexp('^(?!^[0-9a-z]+$)(?!^[0-9A-Z]+$)(?!^[a-zA-Z]+$)[a-zA-Z0-9]{6,20}$', 0, 
+                                'Password must have a combination from letters and numbers and the langth must be 6 to 20.')])
+    password2 = PasswordField('*Reenter your password', validators=[Required()])
+    submit = SubmitField('Confirm')
+    
