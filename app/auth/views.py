231c731aa35b6203df5897a319798bc3de68c506
Zyklop <fzj890909@gmail.com>
2017-04-04 23:35:01 +0200

    add password and email change methods

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,8 @@ from flask_login import current_user, login_required, login_user, logout_user
 from ..models import User
 from ..email import send_mail
 from .. import db
-from .forms import LoginForm, RegisterForm
+from .forms import LoginForm, RegisterForm, PasswordChangeForm, EmailChangeForm, PasswordResetEmailForm, PasswordResetForm
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 @auth.before_app_request
 def before_request():
@@ -76,5 +77,56 @@ def resend_mail():
     flash('A confirmation email has been sent to your email box')
     return redirect(url_for('main.index'))
     
+@auth.route('/password-change', methods=['GET', 'POST'])
+@login_required
+def password_change():
+    form = PasswordChangeForm()
+    if form.validate_on_submit():
+        current_user.password = form.new_password.data
+        db.session.add(current_user)
+        flash('Your password has been successfully changed, please login with new password agin.')
+        send_mail(current_user.email, 'Password Changed', 'mail/auth/password_changed', user=current_user._get_current_object())
+        logout_user()
+        return redirect(url_for('.login'))
+    return render_template('auth/password_change.html', form=form)
 
+@auth.route('/email-change', methods=['GET', 'POST'])
+@login_required
+def email_change():
+    form = EmailChangeForm()
+    if form.validate_on_submit():
+        current_user.confirmed = False
+        token = current_user.generate_confirm_token(email=form.email.data)
+        send_mail(form.email.data, 'Email Change', 'mail/auth/email_change', token=token, user=current_user._get_current_object())
+        flash('Your email has been changed, please login again.')
+        logout_user()
+        return redirect(url_for('.login'))
+    return render_template('auth/email_change.html', form=form)
     
+@auth.route('/password-reset', methods=['GET', 'POST'])
+def password_reset_token():
+    form = PasswordResetEmailForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first_or_404()
+        token = user.generate_confirm_token()
+        send_mail(user.email, 'Password Reset', 'mail/auth/password_reset_token', user=user, token=token)
+        return redirect(url_for('.login'))
+    return render_template('auth/password_reset_token.html', form=form)
+    
+@auth.route('/password-reset/<token>', methods=['GET', 'POST'])
+def password_reset(token):
+    form = PasswordResetForm()
+    s = Serializer(current_app.config['SECRET_KEY'])
+    try:
+        data = s.loads(token)
+    except:
+        flash('Invalid token link')
+        return redirect(url_for('.login'))
+    user = User.query.get_or_404(data.get('confirm'))
+    if form.validate_on_submit():
+        user.password = form.password.data
+        db.session.add(user)
+        flash('Your password has been reseted, please login again.')
+        send_mail(user.email, 'Password Reseted', 'mail/auth/password_reset', user=user)
+        return redirect('.login')
+    return render_template('auth/password_reset.html', form=form)
