b0c482be03e3174a04742bda3d988190a9cb6582
Zyklop <fzj890909@gmail.com>
2017-04-24 00:37:37 +0200

    editing test

--- tests/test_client.py
+++ tests/test_client.py
@@ -24,17 +24,28 @@ class FlaskClientTestCase(unittest.TestCase):
     def test_register_and_login(self):
         response = self.client.post(url_for('auth.register'), data={
             'email': 'admin@example.com',
-            'username': 'Feng',
-            'password': 'katze',
+            'user_name': 'Feng',
+            'password1': 'katze',
+            'password2': 'katze',
+            'name': 'fzj',
+            'location': 'Xuzhou'
         })
-        self.assertTrue(response.status_code == 302)
+        self.assertTrue(b'Your account has been successfully created' in response.data)
+        self.assertTrue(response.status_code == 200)
+        self.assertTrue(User.query.count() > 0)
         
         response = self.client.post(url_for('auth.login'), data={
             'email': 'admin@example.com',
             'password': 'katze'
         }, follow_redirects=True)
-        data = response.get_date(as_text=True)
-        self.assertTrue(re.search('Hello\s+Feng', data))
+        data = response.get_data(as_text=True)
+        self.assertTrue(re.search('Feng', data))
         self.assertTrue('Your account is unconfirmed' in data)
         
+        user = User.query.filter_by(user_name='Feng').first()
+        token = user.generate_confirm_token()
+        response = self.client.get(url_for('auth.confirm', token=token), follow_redirects=True)
+        self.assertTrue(b'Your account has been successfully actived' in response.data)
         
+        response = self.client.get(url_for('auth.logout'), follow_redirects=True)
+        self.assertTrue(b'You have been logged out.' in response.data)
