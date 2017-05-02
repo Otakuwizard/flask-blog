2dab355b5c0589168c6b7454be94835c68d57557
Zyklop <fzj890909@gmail.com>
2017-05-02 15:47:51 +0200

    config

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ class Config:
         
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
     
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
     WTF_CSRF_ENABLED = False
     
 class ProductionConfig(Config):
